507cfd741697cd6eeb602c7b8e2bca6c0ddfbd32
Iwuajoku Melvin <Iwuajoku Melvin>
2016-12-23 12:12:21 +0000

    json dump on register page

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -8,10 +8,26 @@
  * Controller of the backendApp
  */
 angular.module('backendApp')
-  .controller('RegisterCtrl', function () {
+  .controller('RegisterCtrl', function(appSettings, firebaseService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    var register = this;
+    register.register = '';
+    var clockins = firebaseService.initialize(appSettings.firebase.entityKey);
+
+    activate();
+
+    clockins.$watch(function(event) {
+      activate();
+    });
+
+    function activate() {
+      clockins.$loaded()
+        .then(function(data) {
+          register.register = JSON.stringify(data, null, "\t");
+        });
+    }
   });
